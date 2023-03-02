7f0d5a0d9752eb31de4fdfa6d13a9576981f3d96
Unknown <benchu99@hotmail.com>
2023-03-01 16:31:40 -0800

    get_block() quickly reads a block into memory

--- src/gnomAD.jl
+++ src/gnomAD.jl
@@ -3,10 +3,12 @@ module gnomAD
 using AWSS3
 using ProgressMeter
 using PyCall
+using LinearAlgebra
 
 export download_LD_matrices,
     get_all_filenames,
-    hail_block_matrix
+    hail_block_matrix,
+    get_block
 
 include("download.jl")
 include("hailBM.jl")
@@ -15,6 +17,7 @@ include("hailBM.jl")
 # Unlike the @pyimport macro, this does not define a Julia module and members cannot be accessed with s.name.
 # @see https://github.com/JuliaPy/PyCall.jl/issues/328
 const hail = PyNULL()
+const numpy = PyNULL()
 
 function __init__()
     try
@@ -22,7 +25,19 @@ function __init__()
     catch
         error("The gnomAD module is correctly installed, but your python installation is missing the 'hail' module.")
     end
+    try
+        pyimport("numpy")
+    catch
+        error("The gnomAD module is correctly installed, but your python installation is missing the 'numpy' module.")
+    end
     copy!(hail, pyimport("hail.linalg"))
+    copy!(numpy, pyimport("numpy"))
+    
+    # function for reading a block of BlockMatrix
+    py"""
+    def convert(bm, range_start, range_end):
+        return bm[range_start:range_end, range_start:range_end].to_numpy()
+    """
 end
 
 
--- src/hailBM.jl
+++ src/hailBM.jl
@@ -1,20 +1,38 @@
 """
-The Hail Block Matrix format: https://hail.is/docs/0.2/_modules/hail/linalg/blockmatrix.html#BlockMatrix
+Currently a `HailBlockMatrix` is just a python wrapper, where data reading is 
+achieved within python by the Hail software, then the result is passed into
+Julia via PyCall.jl. The `bm` field behaves as a `BlockMatrix` from hail. 
+In the future, a native Julia reader that directly reads
+the compreseed format is desired. 
 
-By default, a gnomAD LD matrix is a banded matrix with band width 1 and blocks of 
-size 4096 by 4096. The blocks look like this:
+The Hail Block Matrix format is described here
+https://hail.is/docs/0.2/_modules/hail/linalg/blockmatrix.html#BlockMatrix
+
+A more specific file description is provided here
+https://discuss.hail.is/t/blockmatrix-specification/3118
 """
-struct HailBlockMatrix <: AbstractMatrix{Float64}
+struct HailBlockMatrix
     bm_files::String
     bm::PyObject
 end
 
+# do not allow getindex for a single dimension
 Base.getindex(x::HailBlockMatrix, i::Int) = 
     error("Can only access HailBlockMatrix using 2 indices (i, j)")
+Base.getindex(x::HailBlockMatrix, ::AbstractRange) = 
+    error("Can only access HailBlockMatrix using 2 dimension index, e.g. bm[1:10, 1:10]")
+
 function Base.getindex(x::HailBlockMatrix, i::Int, j::Int)
-    i == 0 || j == 0 && error("attempt to access matrix at index [$i, $j]")
+    i ≤ 0 || j ≤ 0 && error("attempt to access matrix at index [$i, $j]")
     i > j ? 0.0 : x.bm[i, j]
 end
+function get_block(x::HailBlockMatrix, range_start::Int, range_end::Int)
+    1 ≤ range_start ≤ size(x, 1) || error("range_start out of bounds")
+    1 ≤ range_end ≤ size(x, 1) || error("range_end out of bounds")
+    Σ = py"convert"(x.bm, range_start-1, range_end)
+    LinearAlgebra.copytri!(Σ, 'U')
+end
+
 Base.size(x::HailBlockMatrix, k::Int) = 
     k == 1 ? x.bm.n_rows : k == 2 ? x.bm.n_cols : k > 2 ? 1 : error("Dimension k out of range")
 Base.size(x::HailBlockMatrix) = (size(x, 1), size(x, 2))
